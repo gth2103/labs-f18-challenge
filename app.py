9d48e420737f37fe0995078970e25b3d2bad53d9
Grant Hewlett <gth2103@columbia.edu>
2018-12-03 01:18:59 -0500

    completed ADI Labs application challenge

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
+
 
 
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
 
+
+@app.route('/pokemon/<query>', methods=['GET'])
+def display(query):
+    r = requests.get('https://pokeapi.co/api/v2/pokemon/' + query)
+    data = r.json()
+    for key, value in data.items():
+        if key == 'name':
+            name = value
+        if key == 'id':
+            identify = value
+    if query.isdigit():
+        return render_template('name.html', name=name, identify=identify)
+    return render_template('id.html', name=name.capitalize(), identify=identify)
+
+ 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run()
